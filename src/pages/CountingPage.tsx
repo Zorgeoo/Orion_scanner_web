e5d32506784d598cc46ace65877970279ed1ac2d
Zorgeoo <zjergsen@gmail.com>
2026-01-07 20:24:09 +0800

    fasf

--- src/pages/CountingPage.tsx
+++ src/pages/CountingPage.tsx
@@ -130,7 +130,7 @@ const CountingPage = () => {
       {" "}
       {/* pb-40 ensures content doesn't go under buttons */}
       <div className="max-w-4xl mx-auto">
-        <h1 className="text-3xl font-bold mb-6 text-gray-900">{date}</h1>
+        <h2 className="text-3xl font-bold mb-6 text-gray-900">{date}</h2>
 
         {isLoading && <ListSkeleton />}
 
@@ -166,7 +166,7 @@ const CountingPage = () => {
         )}
       </div>
       {/* Fixed bottom buttons */}
-      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 flex flex-col gap-3 w-[90%] max-w-md">
+      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 z-50 flex flex-col gap-3 w-[90%] max-w-md bg-white">
         <Link to={"/barcodeScanner"}>
           <button className="w-full py-4 bg-yellow-500 text-white font-semibold rounded-full shadow-lg hover:bg-yellow-600 transition-all">
             Баркод уншуулж тоолох
