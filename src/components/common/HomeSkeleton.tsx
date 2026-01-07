d4757add6b6d4abc00cbfb66542b2016128bd3b6
Zorgeoo <zjergsen@gmail.com>
2026-01-08 00:41:32 +0800

    fa

--- src/components/common/HomeSkeleton.tsx
+++ src/components/common/HomeSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function HomeSkeleton() {
   return (
-    <div className="space-y-2 flex flex-col items-center content-center">
+    <div className="space-y-2 flex flex-col items-center content-center pt-16">
       <Skeleton className="h-16 w-[240px]" />
       <Skeleton className="h-16 w-[240px]" />
       <Skeleton className="h-16 w-[240px]" />
