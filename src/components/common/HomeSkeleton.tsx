42e6ea3eb7fe9a936558a2e10a0457fda0cc09db
Zorgeoo <zjergsen@gmail.com>
2026-01-07 15:21:43 +0800

    fda

--- src/components/common/HomeSkeleton.tsx
+++ src/components/common/HomeSkeleton.tsx
@@ -2,10 +2,10 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function HomeSkeleton() {
   return (
-    <div className="space-y-2 flex flex-col items-center">
-      <Skeleton className="h-4 w-[180px]" />
-      <Skeleton className="h-4 w-[180px]" />
-      <Skeleton className="h-4 w-[180px]" />
+    <div className="space-y-2 flex flex-col items-center content-center">
+      <Skeleton className="h-8 w-[180px]" />
+      <Skeleton className="h-8 w-[180px]" />
+      <Skeleton className="h-8 w-[180px]" />
     </div>
   );
 }
