15499744b0c09f5f37a04b5f6a26e6c1a2e817f4
Zorgeoo <zjergsen@gmail.com>
2026-01-09 18:22:42 +0800

    f

--- src/pages/SearchByProductnamePage.tsx
+++ src/pages/SearchByProductnamePage.tsx
@@ -59,11 +59,11 @@ const SearchByProductnamePage = () => {
     <div className="min-h-screen p-4">
       <div className="max-w-2xl mx-auto">
         {/* Header */}
-        <h1 className="px-4 mx-auto text-base font-semibold">
+        <h1 className="px-4 mx-auto text-base font-semibold pb-2">
           Хайж буй барааныхаа нэрийг бичнэ үү!
         </h1>
         {/* Search Input */}
-        <div>
+        <div className="pb-2">
           <div className="relative">
             <svg
               className="absolute left-4 top-1/2 transform -translate-y-1/2 w-5 h-5 text-gray-400"
