cf3790bd9d9c87cffea923f01a9ec3195ccbeb61
Zorgeoo <zjergsen@gmail.com>
2026-01-09 18:06:52 +0800

    f

--- src/components/common/ListSkeleton.tsx
+++ src/components/common/ListSkeleton.tsx
@@ -3,13 +3,13 @@ import { Skeleton } from "@/components/ui/skeleton";
 export default function ListSkeleton() {
   return (
     <div className="space-y-4 flex flex-col items-center content-center">
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
-      <Skeleton className="h-12 w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
+      <Skeleton className="h-[50px] w-full" />
     </div>
   );
 }
