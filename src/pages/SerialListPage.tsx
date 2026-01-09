5e67f8b15e31d2f2edf7ac56fcd10691c6ad9f7b
Zorgeoo <zjergsen@gmail.com>
2026-01-09 21:00:14 +0800

    f

--- src/pages/SerialListPage.tsx
+++ src/pages/SerialListPage.tsx
@@ -1,7 +1,8 @@
 import { getSeriesList } from "@/api/services";
 import { ProductContext } from "@/context/ProductContext";
 import { UserContext } from "@/context/UserContext";
-import { useContext, useEffect } from "react";
+import { SerialModel } from "@/types/SerialModel";
+import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const SerialListPage = () => {
@@ -16,16 +17,19 @@ const SerialListPage = () => {
 
   const { groupNum } = useParams<{ groupNum: string }>();
 
+  const [serials, setSerials] = useState<SerialModel[] | null>(null);
+  const [selectedSerial, setSelectedSerial] = useState<string | null>(null);
+
   useEffect(() => {
     const getSeries = async () => {
       try {
         if (userInfo?.dbase?.dbName && currentCounting && groupNum) {
-          const res = await getSeriesList(
+          const serials = await getSeriesList(
             userInfo?.dbase?.dbName,
             currentCounting?.id,
             groupNum
           );
-          console.log(res);
+          setSerials(serials);
         }
       } catch (error) {
         console.log(error);
@@ -34,9 +38,19 @@ const SerialListPage = () => {
     getSeries();
   }, []);
   return (
-    <div>
-      <div>{currentCounting?.IsBySeriesNumber}</div>
-      <div>{currentCounting?.id}</div>
+    <div className="flex flex-col">
+      {serials &&
+        serials.map((serial, index) => {
+          return (
+            <div
+              // onClick={()=>setSelectedSerial(serial.)}
+              key={index}
+              className="flex flex-col pt-2 border-b-2 border-green-600 text-gray-500 text-sm"
+            >
+              <p className="">{serial.fullSeriesNumber}</p>
+            </div>
+          );
+        })}
     </div>
   );
 };
