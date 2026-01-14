e4038315fda57d4c14b2217945b3a682e7100612
Zorgeoo <zjergsen@gmail.com>
2026-01-14 13:54:48 +0800

    fas

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 // const apiUrl = import.meta.env.VITE_API_URL;
 const api = axios.create({
   baseURL: `https://embainuu.mn/TestApi/`,
-  timeout: 20000,
+  timeout: 40000,
 });
 
 api.interceptors.request.use((config) => {
