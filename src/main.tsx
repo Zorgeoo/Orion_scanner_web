9de499ed2b12d4ebfa78cfced5d0582220dc512c
Zorgeoo <zjergsen@gmail.com>
2026-02-04 12:40:39 +0800

    ffa

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,18 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.js";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import Header from "./components/Header.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <div className="app-root">
         <Header />
         <main className="app-content">
           <App />
         </main>
       </div>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
