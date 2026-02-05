a4bb9920458269287b8b72b0c817f46c4ea133df
Zorgeoo <zjergsen@gmail.com>
2026-02-06 00:06:35 +0800

    ffa

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import "./index.css";
 import App from "./App.js";
 import { HashRouter } from "react-router-dom";
 import Header from "./components/Header.js";
+import ScrollToTop from "./components/ScrollToTop.js";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HashRouter>
+      <ScrollToTop />
       <div className="app-root">
         <Header />
         <main className="app-content">
